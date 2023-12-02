96cc1297aa9b59064f6c1f9260a331a119cf39a6
lczm <zemingchua@gmail.com>
2023-12-02 18:17:54 +0800

    More utility functions

--- zm/utils.cpp
+++ zm/utils.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-static vector<string> get_lines(string file_name) {
+static vector<string> get_lines(const string file_name) {
   string line;
   ifstream file(file_name);
   vector<string> lines;
@@ -11,4 +11,41 @@ static vector<string> get_lines(string file_name) {
   }
 
   return lines;
+}
+
+static vector<string> split(const string s, const char delimiter = ' ') {
+  vector<string> v;
+  string temp;
+  stringstream ss(s);
+  while (getline(ss, temp, delimiter)) {
+    v.push_back(temp);
+  }
+  return v;
+}
+
+static string join(vector<string> v, const char delimiter = ' ') {
+  return accumulate(next(v.begin()), v.end(), v[0],
+                    [&](string a, string b) { return a + delimiter + b; });
+}
+
+static string &ltrim(string &s, const char delimiter = ' ') {
+  s.erase(0, s.find_first_not_of(delimiter));
+  return s;
+}
+
+static string &rtrim(string &s, const char delimiter = ' ') {
+  s.erase(s.find_last_not_of(delimiter) + 1);
+  return s;
+}
+
+static string &trim(string s, const char delimiter = ' ') {
+  return ltrim(rtrim(s, delimiter), delimiter);
+}
+
+static vector<string> ltrim_vec(vector<string> s,
+                                const string delimiter = " ") {
+  int i = 0;
+  while (s[i] == delimiter)
+    i++;
+  return vector<string>(s.begin() + i, s.end());
 }
