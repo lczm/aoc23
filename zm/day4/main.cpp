61457198c6775ea897a610649323a2881788a673
lczm <zemingchua@gmail.com>
2023-12-04 15:45:14 +0800

    Much faster solution

--- zm/day4/main.cpp
+++ zm/day4/main.cpp
@@ -27,17 +27,12 @@ void part1(vector<string> lines) {
 
 void part2(vector<string> lines) {
   unordered_map<int, int> m;
-  unordered_map<int, string> id_m;
-  stack<int> q;
   // Initial process
   for (auto line : lines) {
     auto g = split(line, ':');
     auto id = stoi(split_n_spaces(g[0])[1]);
     m[id]++;
     auto n = split(g[1], '|');
-
-    // for easy look up
-    id_m[id] = g[1];
 
     assert(n.size() == 2);
 
@@ -52,28 +47,7 @@ void part2(vector<string> lines) {
                      inserter(intersect, intersect.begin()));
     if (intersect.size() > 0) {
       for (int i = 1; i <= intersect.size(); i++) {
-        q.push(id + i);
-      }
-    }
-  }
-  while (!q.empty()) {
-    int id = q.top();
-    q.pop();
-    m[id]++;
-    // cout << id << endl;
-    string s = id_m[id];
-    auto n = split(s, '|');
-    auto w = str_to_int_trim(split_n_spaces(n[0]));
-    auto h = str_to_int_trim(split_n_spaces(n[1]));
-    auto winnings = set(w.begin(), w.end());
-    auto have = set(h.begin(), h.end());
-    set<int> intersect;
-    set_intersection(winnings.begin(), winnings.end(), have.begin(), have.end(),
-                     inserter(intersect, intersect.begin()));
-    if (intersect.size() > 0) {
-      for (int i = 1; i <= intersect.size(); i++) {
-        q.push(id + i);
-        // cout << id << " | " << id + i << endl;
+        m[id + i] += m[id];
       }
     }
   }
