9a213be29ec218878a7e3b5f3bac6d66c70c35ad
Zhu Zhanyan <program.nom@gmail.com>
2023-12-07 08:47:04 +0800

    chore(mrzzy): remove unused import in day 4 solution

--- mrzzy/day_4/src/main/java/co/mrzzy/Solution.java
+++ mrzzy/day_4/src/main/java/co/mrzzy/Solution.java
@@ -10,7 +10,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
